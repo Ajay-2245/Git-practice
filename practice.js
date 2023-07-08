a72058a4637d05d4a5d52390c1cca0e1939d90a9
Ajay <vrajaysreenivas003@gmail.com>
2023-07-08 09:24:53 +0530

    Complete Practice.js

---
+++ practice.js
@@ -0,0 +1,10 @@
+function print(a, b)
+{
+    console.log('number of values received :' + arguments.length);
+    for(i =0;i<arguments.length;i++)
+    {
+        console.log(arguments[i]);
+    }
+    
+}
+print(99,199,29,39);  
